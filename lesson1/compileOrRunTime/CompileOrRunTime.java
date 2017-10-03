1534a9924829efcdf2e4496de67c7d639e94f648
Martin Dechev <Martin.Dechev@paysafe.com>
2017-10-03 15:06:27 +0300

    upload lesson 1

---
+++ lesson1/compileOrRunTime/CompileOrRunTime.java
@@ -0,0 +1,10 @@
+// TODO: Decide whether this is a compile-time or run-time error.
+
+public class CompileOrRunTime
+{
+    public static void main(String[] args)
+    {
+        System.out.println(Hello, Sarah);
+        System.out.println("Hello Marto");
+    }
+}
